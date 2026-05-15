5e01bcf1d1443d41c225b54b1b588cc54a156065
bugfreev587 <bugfreev587@gmail.com>
2026-05-14 20:12:26 -0700

    Update public TokenGate repository links

--- frontend/src/views/KeyUsageView.vue
+++ frontend/src/views/KeyUsageView.vue
@@ -375,7 +375,7 @@ const appStore = useAppStore()
 const siteName = computed(() => appStore.cachedPublicSettings?.site_name || appStore.siteName || 'TokenGate')
 const siteLogo = computed(() => appStore.cachedPublicSettings?.site_logo || appStore.siteLogo || '')
 const docUrl = computed(() => appStore.cachedPublicSettings?.doc_url || appStore.docUrl || '')
-const githubUrl = 'https://github.com/Wei-Shaw/sub2api'
+const githubUrl = 'https://github.com/bugfreev587/tokengate'
 
 // ==================== Theme (same as HomeView) ====================
 
